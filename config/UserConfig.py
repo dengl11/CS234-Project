a0602966aca0ca9237aaf9309eb4439e2c3bf8d2
dengl11 <dengl11@stanford.edu>
2017-06-09 23:26:31 -0700

    working on new data

--- config/UserConfig.py
+++ config/UserConfig.py
@@ -1,7 +1,12 @@
 class UserConfig:
 	
 
+	# start_dia_acts = {
+ #    	#'greeting':[],
+ #    	'request':['moviename', 'starttime', 'theater', 'city', 'state', 'date', 'genre', 'ticket', 'numberofpeople']
+	# }
+
+
 	start_dia_acts = {
-    	#'greeting':[],
-    	'request':['moviename', 'starttime', 'theater', 'city', 'state', 'date', 'genre', 'ticket', 'numberofpeople']
+    'request':['destination1', 'flightDate2', 'flightDate1', 'origin1', 'travelers', 'ticket']
 	}
